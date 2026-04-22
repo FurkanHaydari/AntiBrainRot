f1404680286318cc23f6c5e5e706372dcf46c9ee
“Furkan <furkanhydri@gmail.com>
2026-04-22 23:26:54 +0300

    Use KSP instead of Kapt

--- app/src/main/kotlin/com/brainfocus/numberdetective/MainActivity.kt
+++ app/src/main/kotlin/com/brainfocus/numberdetective/MainActivity.kt
@@ -6,6 +6,7 @@ import android.media.SoundPool
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.*
 import androidx.lifecycle.lifecycleScope
 import com.brainfocus.numberdetective.feature.AppNavigation
@@ -37,6 +38,7 @@ class MainActivity : ComponentActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         
         // Initialize SoundManager and sync with settings
